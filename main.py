35107073985a1e5b8e6aea0995630ba193144582
Evenflowz <andreyevenflow@gmail.com>
2025-10-04 08:39:19 +0300

    commit

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+# Serve files from the 'static' directory at the root URL
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Add a route for favicon.ico to serve the emoji SVG
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.svg', mimetype='image/svg+xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
