a96a542e9cfb5e93107538e7df3378e0b54acfe9
Andrey Lopukhov <andreyevenflow@gmail.com>
2026-03-26 09:59:49 +0200

    Update Flask app to use environment port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 from flask_compress import Compress
+import os
 
 # Serve files from the 'static' directory at the root URL
 app = Flask(__name__, static_url_path='')
@@ -15,5 +16,6 @@ def home():
 def favicon_ico():
     return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
